c959cb46f8d5b32a3e01a9e6570fd1bc2c3af175
Tiago Guimarães <tguimaraespinto@gmail.com>
2023-06-17 01:05:31 -0400

    refactor: adjustment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,38 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-900/95"></main>
+    <main className="flex min-h-screen flex-col items-center ">
+      <div className="flex h-[20vh] w-full max-w-screen-xl border-[#748FF026] border-l-2 border-r-2"></div>
+      <div className="flex w-full items-center justify-center py-8 border-t-2 border-b-2 border-[#748FF026]">
+      <Image src="/logomark.svg" 
+      quality={100}
+        width={400} 
+        height={400}
+        alt='banner'
+        className='aspect-auto   '
+      />
+      </div>
+      <div className=" flex flex-1 w-full items-center justify-center border-l-2 border-[#748FF026] border-r-2 max-w-screen-xl p-12">
+      <div>
+      <Image src="/banner.png" 
+      quality={100}
+        width={1280} 
+        height={832}
+        alt='banner'
+        className='aspect-auto   border-4 rounded-[2rem] border-primary'
+      />
+     
+      </div>
+      <Image src="/gradient.png" 
+      quality={100}
+        width={1280} 
+        height={832}
+        alt='banner'
+        className=' w-[100vw] md:translate-y-16 absolute bottom-0 left-0 -z-10'
+      />
+      
+      </div>
+     
+    </main>
   );
 }
---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Chat-io",
+  description:
+    "Chat-io is a chat application that allows you to chat with your friends and family.",
+  url: "https://chat-io.tiagogp.com",
+  ogImage: "https://chat-io.tiagogp.com/og.jpg",
+  links: {
+    twitter: "https://twitter.com/tiagogp_exe",
+    github: "https://github.com/tiagogp-exe",
+  },
+};
