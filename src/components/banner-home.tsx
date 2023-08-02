d73493af9af809895dd7c0fb0498d41edc7b5373
Tiago Guimarães Pinto <tguimaraespinto@gmail.com>
2023-08-02 10:02:21 -0400

    refactor: adjustment

--- src/components/banner-home.tsx
+++ src/components/banner-home.tsx
@@ -8,18 +8,27 @@ export const BannerHome = () => {
 
   return (
     <div className="relative w-full ">
-      <Image
-        src={
-          matches ? "/images/banner-mobile.png" : "/images/banner.png"
-        }
-        quality={100}
-        width={1280}
-        height={832}
-        alt="banner"
-        className={
-          `${matches ? "pl-8" : "px-10"}  relative z-10 h-auto w-full sm:w-auto`
-        }
-      />
+      {
+        matches ? (
+          <Image
+            src="/images/banner-mobile.png"
+            quality={100}
+            width={1280}
+            height={832}
+            alt="banner"
+            className="relative z-10 h-auto w-full pl-8 sm:w-auto"
+          />) : (
+          <Image
+            src={"/images/banner.png"}
+            quality={100}
+            width={1280}
+            height={832}
+            alt="banner"
+            className="relative z-10 h-auto w-full px-10 sm:w-auto"
+          />
+        )
+      }
+
       <div className="absolute left-0 top-0 h-full  w-full blur-3xl ">
         <div className="clip-trapezoid h-full w-full dark:bg-primary "></div>
       </div>
