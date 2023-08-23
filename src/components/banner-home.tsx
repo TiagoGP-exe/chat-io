f9ddd427817c18f78ed6a320cdea61f1c2bf2b2b
Tiago Guimarães Pinto <tguimaraespinto@gmail.com>
2023-08-23 13:17:56 -0400

    refactor: adjustment

--- src/components/banner-home.tsx
+++ src/components/banner-home.tsx
@@ -1,34 +1,8 @@
 "use client"
 
-import { useWindowSize } from '@/hooks/use-window-size'
 import Image from 'next/image'
 
 export const BannerHome = () => {
-  const { width } = useWindowSize()
-
-  const matches = width > 640
-
-  if (matches) {
-    return (
-
-      <div className="relative w-full ">
-        <Image
-          src="/images/banner.png"
-          quality={100}
-          width={1280}
-          height={832}
-          alt="banner desktop"
-          className="relative z-10 h-auto w-full px-10 sm:w-auto"
-        />
-
-        <div className="absolute left-0 top-0 h-full  w-full blur-3xl ">
-          <div className="clip-trapezoid h-full w-full dark:bg-primary "></div>
-        </div>
-      </div>
-    )
-  }
-
-
   return (
     <div className="relative w-full ">
       <Image
@@ -37,7 +11,16 @@ export const BannerHome = () => {
         width={1280}
         height={832}
         alt="banner desktop"
-        className="relative z-10 h-auto w-full pl-10 sm:w-auto"
+        className="relative z-10  h-auto w-full pl-10 sm:hidden sm:w-auto"
+      />
+
+      <Image
+        src="/images/banner.png"
+        quality={100}
+        width={1280}
+        height={832}
+        alt="banner desktop"
+        className="relative z-10 hidden h-auto w-full px-10 sm:block sm:w-auto"
       />
 
       <div className="absolute left-0 top-0 h-full  w-full blur-3xl ">
