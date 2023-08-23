91e4331ad28377ff05bf0a7cd79600dca0aa0d1f
Tiago Guimarães Pinto <tguimaraespinto@gmail.com>
2023-08-23 00:37:05 -0400

    refactor: adjustment

--- src/components/banner-home.tsx
+++ src/components/banner-home.tsx
@@ -6,19 +6,10 @@ import Image from 'next/image'
 export const BannerHome = () => {
   const matches = useMediaQuery('(min-width: 640px)')
 
-
   return (
     <div className="relative w-full ">
       {
-        !matches ? (
-          <Image
-            src="/images/banner-mobile.png"
-            quality={100}
-            width={1280}
-            height={832}
-            alt="banner"
-            className="relative z-10 h-auto w-full pl-8 sm:w-auto"
-          />) : (
+        matches ? (
           <Image
             src={"/images/banner.png"}
             quality={100}
@@ -26,6 +17,15 @@ export const BannerHome = () => {
             height={832}
             alt="banner"
             className="relative z-10 h-auto w-full px-10 sm:w-auto"
+          />
+        ) : (
+          <Image
+            src="/images/banner-mobile.png"
+            quality={100}
+            width={1280}
+            height={832}
+            alt="banner"
+            className="relative z-10 h-auto w-full pl-8 sm:w-auto"
           />
         )
       }
